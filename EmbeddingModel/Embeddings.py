67772ef093feef42e2fd116f5d6f5fe3c2f5d885
Rushabh Mowade <rushabhmwd@gmail.com>
2026-05-23 20:01:27 +0530

    Document embedded

--- EmbeddingModel/Embeddings.py
+++ EmbeddingModel/Embeddings.py
@@ -3,7 +3,8 @@ from langchain_huggingface import HuggingFaceEmbeddings
 embeddings = HuggingFaceEmbeddings(model_name='sentence-transformers/all-MiniLM-L6-v2')
 
 text = " Delhi is the capital of india"
-
+#docs=["hello boy","boy hello"]
 vector = embeddings.embed_query(text)
+#vector = embeddings.embed_documents(docs)
 
 print(str(vector))
