fab32b4413cda3e35d3bb2cb30d0c9a80dcaaad0
Rushabh Mowade <rushabhmwd@gmail.com>
2026-05-23 23:35:17 +0530

    prompt generator

--- Prompts/prompt.py
+++ Prompts/prompt.py
@@ -1,7 +1,8 @@
 import os
 from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
 import streamlit as st
-from langchain_core.prompts import PromptTemplate
+from langchain_core.prompts import load_prompt
+
 
 my_token = "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"
 os.environ["HUGGINGFACEHUB_API_TOKEN"] = my_token
@@ -13,28 +14,17 @@ p = st.selectbox( "Select Research Paper Name", ["Attention Is All You Need", "B
 s = st.selectbox( "Select Explanation Style", ["Beginner-Friendly", "Technical", "Code-Oriented", "Mathematical"] ) 
 
 l = st.selectbox( "Select Explanation Length", ["Short (1-2 paragraphs)", "Medium (3-5 paragraphs)", "Long (detailed explanation)"] )
+#####################################
+import json
+from langchain_core.load import loads
 
-template = PromptTemplate(
-    template="""
-Please summarize the research paper titled "{paper_input}" with the following specifications:
-Explanation Style: {style_input}  
-Explanation Length: {length_input}  
-1. Mathematical Details:  
-   - Include relevant mathematical equations if present in the paper.  
-   - Explain the mathematical concepts using simple, intuitive code snippets where applicable.  
-2. Analogies:  
-   - Use relatable analogies to simplify complex ideas.  
-If certain information is not available in the paper, respond with: "Insufficient information available" instead of guessing.  
-Ensure the summary is clear, accurate, and aligned with the provided style and length.
-""",
-input_variables=['paper_input', 'style_input','length_input']
-)
+with open('Prompts/template.json', 'r', encoding='utf-8') as f:
+    json_string = f.read()
 
-prompt=template.invoke({
-    'paper_input':p,
-    'style_input':s,
-    'length_input':l
-})
+template = loads(json_string)
+#######################################
+
+
 
 llm = HuggingFaceEndpoint(
     repo_id="meta-llama/Llama-3.2-1B-Instruct",
@@ -47,5 +37,10 @@ llm = HuggingFaceEndpoint(
 chat_model = ChatHuggingFace(llm=llm)
 
 if st.button("Summarize"):
-    result = chat_model.invoke(prompt)
+    chain= template | chat_model
+    result = chain.invoke({
+    'paper_input':p,
+    'style_input':s,
+    'length_input':l
+})
     st.write(result.content)
