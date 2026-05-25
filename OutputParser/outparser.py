5d610b95fc96eafc476ab367f1a68d3010e8ab5e
Rushabh Mowade <rushabhmwd@gmail.com>
2026-05-25 16:40:54 +0530

    PydanticOutputParser

--- OutputParser/outparser.py
+++ OutputParser/outparser.py
@@ -1,18 +1,23 @@
 from langchain_core.prompts import PromptTemplate
 from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
 from dotenv import load_dotenv
-from langchain_core.output_parsers import StrOutputParser, JsonOutputParser
+from langchain_core.output_parsers import StrOutputParser, JsonOutputParser, PydanticOutputParser
+from langchain_classic.output_parsers import StructuredOutputParser, ResponseSchema
+from pydantic import BaseModel,Field
 
 load_dotenv()
 
 llm = HuggingFaceEndpoint(
-    repo_id="meta-llama/Llama-3.2-1B-Instruct",
+    repo_id="deepseek-ai/DeepSeek-V4-Flash",
     task="conversational",
     temperature=0.5,
 )
 
 
 model = ChatHuggingFace(llm=llm)
+
+
+"""
 
 template1 = PromptTemplate(
     template='write the a report on {topic}',
@@ -22,7 +27,6 @@ template2 = PromptTemplate(
     template='write the a 5 line summary on {text}',
     input_variables=['text']
 )
-"""
 
 prompt1=template1.invoke({'topic':'Balck Hole'})
 result = model.invoke(prompt1)
@@ -31,6 +35,7 @@ print(model.invoke(prompt2).content)
 
 
 """
+
 """
 
 parser = StrOutputParser()
@@ -40,12 +45,64 @@ print(result)
 
 """
 
+"""
+
+
 parser = JsonOutputParser()
 template = PromptTemplate(
     template='Give me some sunshine give me some prays \n {format_instruction}',
     input_variables=[],
     partial_variables={'format_instruction':parser.get_format_instructions()}
 )
-prompt=template.format()
+#prompt=template.format()
+#result = model.invoke(prompt)
+#final = parser.parse(result.content)
+chain = template | model | parser
+final = chain.invoke({})
+print(final)
+
+
+"""
+
+"""
+
+
+# Structured Output
+schema = [
+    ResponseSchema(name='Fact 1',description='Fact 1 about the topic'),
+    ResponseSchema(name='Fact 2',description='Fact 2 about the topic'),
+    ResponseSchema(name='Fact 2',description='Fact 3 about the topic')
+]
+parser = StructuredOutputParser.from_response_schemas(schema)
+
+template = PromptTemplate(
+    template='Give me 3 facts about the {topic} \n {format_instruction}',
+    input_variables=["topic"],
+    partial_variables={'format_instruction':parser.get_format_instructions()}
+)
+chain = template | model | parser
+result = chain.invoke({"topic":"Black Hole"})
+print(result)
+
+"""
+
+class Person(BaseModel):
+    name: str = Field(...,description="Name of the Person")
+    age : int = Field(...,gt=18,description="Age of the person]")
+    city : str = Field(...,description="City")
+
+
+parser = PydanticOutputParser(pydantic_object=Person)
+
+template = PromptTemplate(
+    template='Generate the name ,age and city of {topic} Person \n {format_instruction}',
+    input_variables=["topic"],
+    partial_variables={'format_instruction':parser.get_format_instructions()}
+)
+
+prompt = template.invoke({'topic': 'German'})
 result = model.invoke(prompt)
-print(parser.parse(result.content))
+final = parser.parse(result.content)
+#chain = template | model | parser
+#final = chain.invoke({'topic': 'German'})  
+print(final)
